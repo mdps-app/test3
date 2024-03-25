69cd447bc90725123a0e05417fe2a41b20038e9c
shiki <kishintorii@gmail.com>
2024-03-25 09:58:26 +0900

    📁 save

---
+++ src/routes/account/new/+page.svelte
@@ -0,0 +1,25 @@
+<div>
+    <h1>サインアップ</h1>
+    <form method="POST">
+      <label for="name">ユーザー名</label>
+      <input type="text" name="name" />
+  
+      <label for="email">メールアドレス</label>
+      <input type="text" name="email" />
+  
+      <label for="password">パスワード</label>
+      <input type="password" name="password" />
+  
+      <label for="confirmPassword">パスワード（確認）</label>
+      <input type="password" name="confirm"/>
+          
+      <div><button>サインアップ</button></div>
+    </form>
+  </div>
+  
+  <style>
+    input {
+      display: block;
+      margin-bottom: 10px;
+    }
+  </style>
